2acdab41c4a73f8db1ed50b493a5b34bfc779823
Anzhelika Kirichenko <evgenkir2015@gmail.com>
2026-05-28 02:15:19 +0300

    ut pred

--- src/model/universal_transformer.py
+++ src/model/universal_transformer.py
@@ -1,26 +1,35 @@
 from torch import nn
 import torch
+import math
 
-from src.model.layers import Block, get_2d_sincos_pos_embed
-from src.model.utils import trunc_normal_
+from .layers import *
+from .utils import *
 
 
-class UniversalTransformerBlock(nn.Module):
-    def __init__(self, num_patches, predictor_dim, encoder_dim, num_heads, p_dropout=0.0, num_steps=4, time_emb=False, act=False, max_steps=None, epsilon=None):
-        """
-        Args:
-            predictor_dim (int): size of predictor's hidden dimension
-            encoder_dim (int): size of encoder's hidden dimension
-            num_heads (int): number of heads to use in Attention
-            p_dropout (float): probability of an element to be zeroed
-            num_steps (int): number of times to run the Transformer block
-            time_emb (bool): whether to use time embedding
-            act (bool): whether to use ACT 
-            max_steps (int): maximum possible steps a token goes through Transformer block
-            epilon (float): ACT epsilon
-        """
+class UniversalTransformerPredictor(nn.Module):
+    def __init__(
+        self,
+        num_patches,
+        act=False,
+        time_emb=False,
+        epsilon=None,
+        num_steps=4,
+        max_steps=4,
+        embed_dim=768,
+        predictor_embed_dim=384,
+        depth=6,
+        num_heads=12,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.0,
+        norm_layer=nn.LayerNorm,
+        init_std=0.02,
+        **kwargs
+    ):
         super().__init__()
-        self.mask_token = nn.Parameter(torch.zeros(1, 1, predictor_dim))
 
         self.act = act
         self.time_emb = time_emb
@@ -29,48 +38,82 @@ class UniversalTransformerBlock(nn.Module):
 
         if act:
             self.epsilon = epsilon
-            self.halting_linear = nn.Linear(predictor_dim, 1)
+            self.halting_linear = nn.Linear(predictor_embed_dim, 1)
 
-        self.layernorm = nn.LayerNorm(predictor_dim)
-        self.dropout = nn.Dropout(p_dropout)
-        self.block = Block(predictor_dim, num_heads, qkv_bias=True)
-
-        self.predictor_pos_embed = nn.Parameter(torch.zeros(1, num_patches, predictor_dim),
+        self.predictor_embed = nn.Linear(embed_dim, predictor_embed_dim, bias=True) # перескакиваем в пространство меньшей размерности predictor_embed_dim
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, predictor_embed_dim)) # [1, 1, E], токены масок которые будем предсказывать
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # Чем глубже - тем чаще дропаутим слои, см class DropPath
+        # --
+        self.predictor_pos_embed = nn.Parameter(torch.zeros(1, num_patches, predictor_embed_dim),
                                                 requires_grad=False)
         predictor_pos_embed = get_2d_sincos_pos_embed(self.predictor_pos_embed.shape[-1],
                                                       int(num_patches**.5),
                                                       cls_token=False)
-        self.predictor_pos_embed.data.copy_(torch.from_numpy(predictor_pos_embed).float().unsqueeze(0))
+        self.predictor_pos_embed.data.copy_(torch.from_numpy(predictor_pos_embed).float().unsqueeze(0)) # Кринжово создали пос эмбеддинг
+        # --
+        
+        self.block = Block(predictor_embed_dim, num_heads, qkv_bias=True)
+        self.predictor_norm = norm_layer(predictor_embed_dim) # нормализация на чилле
+        self.predictor_proj = nn.Linear(predictor_embed_dim, embed_dim, bias=True) # проекция на чилле обратно в пространство большей размерности
+        # ------
 
-        self.step_embeddings = nn.Parameter(torch.zeros(self.num_steps, 1, predictor_dim))
+        self.step_embeddings = nn.Parameter(torch.zeros(self.num_steps, 1, predictor_embed_dim))
         trunc_normal_(self.step_embeddings, std=0.02)
 
-        self.pred_proj = nn.Linear(in_features=encoder_dim, out_features=predictor_dim)
-        self.enc_proj = nn.Linear(in_features=predictor_dim, out_features=encoder_dim)
+        self.init_std = init_std # для инициализации весов
+        trunc_normal_(self.mask_token, std=self.init_std)
+        self.apply(self._init_weights)
+        self.fix_init_weight()
+
+    def fix_init_weight(self):
+        def rescale(param, layer_id):
+            param.div_(math.sqrt(2.0 * layer_id))
+
+        for layer_id, layer in enumerate(self.predictor_blocks):
+            rescale(layer.attn.proj.weight.data, layer_id + 1)
+            rescale(layer.mlp.fc2.weight.data, layer_id + 1)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=self.init_std)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=self.init_std)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x, masks_x, masks):
-        """
-        Args:
-            x (Tensor): context embeddings of the images' patches of shape [B, num_patches, enc_dim]
-            masks_x (Tensor): patch indices of the context (images' visible parts) of shape [N_ctxt]
-            masks (Tensor): patch indices of targets of shape [N_trgt]
-        Returns a tensor of predicted target embeddings
-        """
+        assert (masks is not None) and (masks_x is not None), 'Cannot run predictor without mask indices'
+
+        if not isinstance(masks_x, list):
+            masks_x = [masks_x]
+
+        if not isinstance(masks, list):
+            masks = [masks]
+
+        # -- Batch Size
         B = len(x) // len(masks_x)
-        # Prepating context embeddings
-        x = self.pred_proj(x)
+
+        # -- map from encoder-dim to pedictor-dim
+        x = self.predictor_embed(x)
+
+        # -- add positional embedding to x tokens
         x_pos_embed = self.predictor_pos_embed.repeat(B, 1, 1)
-        x += self.apply_masks(x_pos_embed, masks_x)
+        x += apply_masks(x_pos_embed, masks_x)
 
-        N_ctxt = x.size(1)
+        _, N_ctxt, D = x.shape
 
-        # Preparing pos embeddings for targets
+        # -- concat mask tokens to x
         pos_embs = self.predictor_pos_embed.repeat(B, 1, 1)
-        pos_embs = self.apply_masks(pos_embs, masks)
-        pos_embs = self.repeat_interleave_batch(pos_embs, B, repeat=len(masks_x))
-
-        # Preparing tokens for target (context + pos)
+        pos_embs = apply_masks(pos_embs, masks)
+        pos_embs = repeat_interleave_batch(pos_embs, B, repeat=len(masks_x))
+        # --
         pred_tokens = self.mask_token.repeat(pos_embs.size(0), pos_embs.size(1), 1)
+        # --
         pred_tokens += pos_embs
         x = x.repeat(len(masks), 1, 1)
         x = torch.cat([x, pred_tokens], dim=1)
@@ -121,31 +164,13 @@ class UniversalTransformerBlock(nn.Module):
                 "act_loss": act_loss
             }
 
-        x = self.layernorm(x)
+
+        x = self.predictor_norm(x)
+
+        # -- return preds for mask tokens
         x = x[:, N_ctxt:]
-        x = self.enc_proj(x)
+        x = self.predictor_proj(x)
 
         return {
-            "predictions": x,
-            "act_loss" : 0.0
-        }    
-    
-    def apply_masks(self, x, masks):
-        """
-        Args:
-            x (Tensor): data of shape [B (batch-size), N (num-patches), D (feature-dim)]
-            masks (list): list containing indices of patches to keep
-        """
-        all_x = []
-        for m in masks:
-            mask_keep = m.unsqueeze(-1).repeat(1, 1, x.size(-1))
-            all_x += [torch.gather(x, dim=1, index=mask_keep)]
-        return torch.cat(all_x, dim=0)
-    
-    def repeat_interleave_batch(self, x, B, repeat):
-        N = len(x) // B
-        x = torch.cat([
-            torch.cat([x[i*B:(i+1)*B] for _ in range(repeat)], dim=0)
-            for i in range(N)
-        ], dim=0)
-        return x
+            "predictions": x
+        }
