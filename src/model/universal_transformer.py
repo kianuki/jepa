4c7e224efacb348ccc2de93e9fac2ffc9952ca89
Anzhelika Kirichenko <evgenkir2015@gmail.com>
2026-05-28 02:33:18 +0300

    fix 2

--- src/model/universal_transformer.py
+++ src/model/universal_transformer.py
@@ -68,9 +68,8 @@ class UniversalTransformerPredictor(nn.Module):
         def rescale(param, layer_id):
             param.div_(math.sqrt(2.0 * layer_id))
 
-        for layer_id, layer in enumerate(self.block):
-            rescale(layer.attn.proj.weight.data, layer_id + 1)
-            rescale(layer.mlp.fc2.weight.data, layer_id + 1)
+        rescale(self.block.attn.proj.weight.data, 1)
+        rescale(self.block.mlp.fc2.weight.data, 1)
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
